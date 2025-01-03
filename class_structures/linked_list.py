2fd0bee41c013a56404c86c9bcc59dd7f3510ee5
faiaz112 <jualam@ttu.edu>
2025-01-02 22:27:45 -0500

    Added linked list in python

--- class_structures/linked_list.py
+++ class_structures/linked_list.py
@@ -18,7 +18,7 @@ class LinkedList:
             print(temp.value)
             temp=temp.next
 
-    #Appending a value to the linked list
+    #Appending a value to the end of the linked list
     def append(self,value):
         new_node=Node(value)
         #if it's an empty linked list
@@ -40,7 +40,7 @@ class LinkedList:
         #Now writing the code if there's one or multiple nodes in the linked list
         temp=self.head
         pre=self.head
-        while(temp.next):#this can also be written as (if temp.next is not None)
+        while(temp.next):#this can also be written as (While temp.next is not None)
             pre=temp
             temp=temp.next
         self.tail=pre
@@ -51,6 +51,21 @@ class LinkedList:
             self.head=None
             self.tail=None
         return temp
+    
+    #Appending a node to the beginning of the linked list
+    def prepend(self,value):
+        new_node=Node(value)
+        #if the linked list is empty
+        if(self.length==0):
+            self.head=new_node
+            self.tail=new_node
+        #if there is one or more nodes present in the linked list
+        else:
+            new_node.next=self.head
+            self.head=new_node
+        self.length+=1
+        return True
+        
 
 #Creating a linked list and printing it's value
 print("Creating the Linked list")
@@ -67,5 +82,10 @@ my_linked_list.print_list()
 #popping the last node from the linked list
 print("Popping the last node from the linked list")
 my_linked_list.pop()
+my_linked_list.print_list()
+
+#prepending a node to the linked list
+print("Prepending a node to the linked list")
+my_linked_list.prepend(3)
 my_linked_list.print_list()
 
