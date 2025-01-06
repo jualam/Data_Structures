62b18ead713b6b851005600c6cc7cba9a54234ab
faiaz112 <jualam@ttu.edu>
2025-01-06 00:45:06 -0500

    New prac

--- class_structures/doubly_linked_list.py
+++ class_structures/doubly_linked_list.py
@@ -67,7 +67,39 @@ class DoublyLinkedList:
         return True
     
     #popping a node from the beginning of the list
+    def pop_first(self):
+        #Empty list
+        if (self.length==0):
+            return None
+        temp=self.head
+        #if there is only one node in the list
+        if (self.length==1):
+            self.head=None
+            self.tail=None
+        else:
+            self.head=self.head.next
+            self.head.prev=None
+            temp.next=None
+        self.length-=1
+        return temp
+    
+    #get an item in a particular index
+    def get(self,index):
+        if index<0 or index>=self.length:
+            return None
+        #if it's in the first half,then iterating from head is more efficient
+        if index<self.length/2:
+            temp=self.head
+            for _ in range(index):
+                temp=temp.next
+        else:
+            #if it's in the second half,then iterating from tail is more efficient
+            temp=self.tail
+            for _ in range(self.length-1,index,-1):
+                temp=temp.prev
+        return temp
 
+        
     
 #Creating a doubly linked list using the constructor
 print("Creating the doubly linked list")
@@ -80,6 +112,9 @@ my_dll.print_list()
 #appending a node to the end of the list
 print("\nAppending a node to the end of the list")
 my_dll.append(7)
+my_dll.append(8)
+my_dll.append(81)
+my_dll.append(23)
 my_dll.print_list()
 
 #popping a node from the end of the list
@@ -91,6 +126,17 @@ my_dll.print_list()
 print("\nAppending a node to the start of the list")
 my_dll.prepend(5)
 my_dll.print_list()
+
+#poppinjg anode from the beginnng of the list
+print("\nPopping a node from the beginning of the list")
+print(my_dll.pop_first())#returning the popped node adress
+my_dll.print_list()
+
+#getting a node from a particular index from the list
+print("\nGetting a node from a particular index from the list")
+print(my_dll.get(1).value)
+print(my_dll.get(2).value)
+
 
 
 
